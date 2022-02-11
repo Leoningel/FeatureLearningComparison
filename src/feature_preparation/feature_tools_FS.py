bea322a69116920e9b51de4121a81010c55ca15e
Leon Ingelse <leoningelse@gmail.com>
2022-02-11 16:01:00 +0000

    Update feature_tools_FS.py

--- src/feature_preparation/feature_tools_FS.py
+++ src/feature_preparation/feature_tools_FS.py
@@ -31,16 +31,16 @@ class FeatureToolsFS(FeatureLearningMethod):
     method = RemoveHighlyCorrelatedFeatures
     
     def mapping(self, data):
-        # features = data.columns
-        # es = ft.EntitySet(id="0")
-        # es = es.add_dataframe(
-        #     dataframe_name="boom_bikes",
-        #     dataframe=data,
-        #     index="instant"
-        # )
-        # fm, features = ft.dfs(entityset=es, target_dataframe_name="boom_bikes", max_depth=1)
-        # new_fm, _ = remove_highly_correlated_features(fm,features=features)
-        return data
+        features = data.columns
+        es = ft.EntitySet(id="0")
+        es = es.add_dataframe(
+            dataframe_name="boom_bikes",
+            dataframe=data,
+            index="instant"
+        )
+        fm, features = ft.dfs(entityset=es, target_dataframe_name="boom_bikes", max_depth=1)
+        new_fm, _ = remove_highly_correlated_features(fm,features=features)
+        return new_fm
     
     def __str__(self) -> str:
         return "FeatureTools_FS"
