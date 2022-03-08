4f8c0ec35112281a86d1b3d9130d7d78627f7a27
Leon Ingelse <leoningelse@gmail.com>
2022-03-08 17:11:59 +0000

    Update m3gp_jb.py

--- src/feature_preparation/search_based/m3gp_jb.py
+++ src/feature_preparation/search_based/m3gp_jb.py
@@ -11,16 +11,17 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class M3GPFL_JB(BaseEstimator, TransformerMixin):
-    def __init__(self, max_depth=15) -> None:
+    def __init__(self, max_depth=15, elitism_size=5) -> None:
         self.feature_mapping = None
         self.max_depth = max_depth
+        self.elitism_size = elitism_size
 
     def fit(self,X,y=None,seed=42):
         m3gp = M3GP_alg(
                     population_size=500,
                     max_generation=500,
                     limit_depth=self.max_depth,
-                    elitism_size=5,
+                    elitism_size=self.elitism_size,
                     max_initial_depth=self.max_depth,
                     model_name="DecisionTreeRegressor", 
                     fitnessType="MSE",
@@ -38,7 +39,10 @@ class M3GPFL_JB(BaseEstimator, TransformerMixin):
         return Xt
 
 class M3GP_JB(FeatureLearningMethod):
-    param_grid: Union[dict, list] = { "feature_learning__max_depth": [ 15, 20 ]}
+    param_grid: Union[dict, list] = { 
+                            "feature_learning__max_depth": [ 5, 10, 15, 20 ],
+                            "feature_learning__elitism_size": [ 1, 5, 25, 100 ]
+                            }
     method = M3GPFL_JB
     
     def __str__(self) -> str:
