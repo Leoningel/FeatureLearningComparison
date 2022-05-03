bd47c4952ddd1741d120619d6037353afeeb9632
Leon Ingelse <leoningelse@gmail.com>
2022-05-03 17:16:26 +0100

    Update aggregations.py

--- src/feature_preparation/search_based/grammar/aggregations.py
+++ src/feature_preparation/search_based/grammar/aggregations.py
@@ -38,4 +38,4 @@ class Average(BuildingBlock):
         return aggregates
     
     def __str__(self):
-        return f"average({self.aggregation_col} for historic {self.col})"
+        return f"average(\"{self.aggregation_col}\" for historic {self.col})"
