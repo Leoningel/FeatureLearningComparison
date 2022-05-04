403fdad62304e77fac4c4743fc36de7066808e96
Leon Ingelse <leoningelse@gmail.com>
2022-05-04 20:09:40 +0100

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -18,12 +18,12 @@ from evaluation.visualization import plot_combined_barplot_comparison, plot_sepa
 from feature_preparation.core import FeatureLearningMethod, FeatureLearningOptimization
 from feature_preparation.classical_methods.feature_tools_FS import FeatureToolsFS
 from feature_preparation.classical_methods.no_feature_construction import NoFeatureLearning
-from feature_preparation.classical_methods.principle_component_analysis import Principle_CA
+from feature_preparation.classical_methods.principle_component_analysis import PrincipleCA
 from feature_preparation.search_based.random_search import RandomSearchFS
 from feature_preparation.search_based.traditional_gp import TraditionalGP
 from feature_preparation.search_based.m3gp_jb import M3GP_JB
 from feature_preparation.search_based.m3gp_gengy import M3GP_Gengy
-from feature_preparation.search_based.dk_m3gp import M3GP_DK_Gengy
+from feature_preparation.search_based.dk_m3gp import DK_M3GP
 from feature_preparation.search_based.dka_m3gp import DKA_M3GP
 from model_generation.models import DecisionTree, RandomForest, MLP, SVM, Model
 import utils
@@ -33,8 +33,8 @@ CROSS_VALIDATION_GROUPS = 2
 TRAIN_PROPORTION = 0.75
  
 models : List[Model] = [ DecisionTree(), RandomForest(), MLP(), SVM() ]
-feature_learnings : List[FeatureLearningMethod] = [ DKA_M3GP() ]
-# feature_learnings : List[FeatureLearningMethod] = [ DKA_M3GP(), M3GP_DK_Gengy(), M3GP_Gengy(), M3GP_JB(), TraditionalGP(), RandomSearchFS(), Principle_CA(), FeatureToolsFS(), NoFeatureLearning() ]
+feature_learnings : List[FeatureLearningMethod] = [ FeatureToolsFS() ]
+# feature_learnings : List[FeatureLearningMethod] = [ DKA_M3GP(), DK_M3GP(), M3GP_Gengy(), M3GP_JB(), TraditionalGP(), RandomSearchFS(), PrincipleCA(), FeatureToolsFS(), NoFeatureLearning() ]
 
 
 
