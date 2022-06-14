23dc806ab12b410450f5382191bbc09c25b5c731
Leon Ingelse <leoningelse@gmail.com>
2022-06-14 14:15:00 +0100

    New lineplots and fixed all bugs

--- src/evaluation/visualization/specifics.py
+++ src/evaluation/visualization/specifics.py
@@ -1,3 +1,4 @@
+from typing import List
 import pandas as pd
 import seaborn as sns
 import glob
@@ -9,12 +10,11 @@ from model_generation.models import Model
 
 
 
-
-
-
 def visualise_single_file(feature_learning: FeatureLearningMethod, seed: int, split: float, model: Model, column: str = 'fitness'):
     df = pd.read_csv(f"{gv.RESULTS_FOLDER}/{feature_learning}/seed={seed}_model={model}_split={split}.csv")
     df = df[[column, "number_of_the_generation"]]
+    df = df.groupby(['number_of_the_generation']).min()
+    df = df.reset_index()
     
     plt.close()
     sns.set_style({"font.family": "serif"})
@@ -23,7 +23,9 @@ def visualise_single_file(feature_learning: FeatureLearningMethod, seed: int, sp
     a = sns.lineplot(data=df, x = "number_of_the_generation", y = column)
 
     a.set_title(f"{feature_learning} {column}")
-    plt.savefig(f"plots/test_single_file/seed={seed}_model={model}_split={split}.pdf")
+    path = f"plots/test_single_file/{feature_learning}/seed={seed}_model={model}_split={split}.pdf"
+    plt.savefig(path)
+    print(f"Saved figure to {path}.")
     
 
 def visualise_all_seeds(feature_learning: FeatureLearningMethod, split: float, model: Model, column: str = 'fitness'):
@@ -36,7 +38,8 @@ def visualise_all_seeds(feature_learning: FeatureLearningMethod, split: float, m
         print(f"{round((idx/(len(all_files) + 1)) * 100,1)} %", end='\r')
         df = pd.read_csv(filename, index_col=None, header=0)
         df = df[[column, "number_of_the_generation"]]
-        df.groupby(['number_of_the_generation']).min()
+        df = df.groupby(['number_of_the_generation']).min()
+        df = df.reset_index()
         li.append(df)
     
     df = pd.concat(li, axis=0, ignore_index=True)
@@ -48,8 +51,47 @@ def visualise_all_seeds(feature_learning: FeatureLearningMethod, split: float, m
     a = sns.lineplot(data=df, x = "number_of_the_generation", y = column)
 
     a.set_title(f"{feature_learning} {column}")
-    plt.savefig(f"plots/test_single_file/all_seeds_model={model}_split={split}.pdf")
-    print(f"Done!!!")
+    path = f"plots/test_single_file/{feature_learning}/all_seeds_model={model}_split={split}.pdf"
+    plt.savefig(path)
+    print(f"Saved figure to {path}.")
+
+def visualise_all_seeds_compare_splits(feature_learning: FeatureLearningMethod, splits: List[float], model: Model, column: str = 'fitness'):
+
+    li = []
+
+    for split in splits:
+        all_files = glob.glob(f"{gv.RESULTS_FOLDER}/{feature_learning}/seed=*_model={model}_split={split}.csv")
+
+        if split == 0.75:
+            split = 'whole dataset'
+        
+        for idx, filename in enumerate(all_files):
+            print(f"{round((idx/(len(all_files) + 1)) * 100,1)} %", end='\r')
+            df = pd.read_csv(filename, index_col=None, header=0)
+            df = df[[column, "number_of_the_generation"]]
+            df = df.groupby(['number_of_the_generation']).min()
+            df['split'] = str(split)
+            df = df.reset_index()
+            li.append(df)
+        
+    df = pd.concat(li, axis=0, ignore_index=True)
+    
+    plt.close()
+    sns.set_style({"font.family": "serif"})
+    sns.set(font_scale=0.75)
+
+    a = sns.lineplot(
+            data=df,
+            x = "number_of_the_generation",
+            y = column,
+            hue = 'split'
+            )
+
+    a.set_title(f"{feature_learning} {column}")
+    path = f"plots/test_single_file/{feature_learning}/all_seeds_model={model}_splits_comparison.pdf"
+    plt.savefig(path)
+    print(f"Saved figure to {path}.")
+        
 
 def visualise_all_seeds_all_splits(feature_learning: FeatureLearningMethod, model: Model, column: str = 'fitness'):
 
@@ -61,7 +103,8 @@ def visualise_all_seeds_all_splits(feature_learning: FeatureLearningMethod, mode
         print(f"{round((idx/(len(all_files) + 1)) * 100,1)} %", end='\r')
         df = pd.read_csv(filename, index_col=None, header=0)
         df = df[[column, "number_of_the_generation"]]
-        df.groupby(['number_of_the_generation']).min()
+        df = df.groupby(['number_of_the_generation']).min()
+        df = df.reset_index()
         li.append(df)
     
     df = pd.concat(li, axis=0, ignore_index=True)
@@ -73,8 +116,9 @@ def visualise_all_seeds_all_splits(feature_learning: FeatureLearningMethod, mode
     a = sns.lineplot(data=df, x = "number_of_the_generation", y = column)
 
     a.set_title(f"{feature_learning} {column}")
-    plt.savefig(f"plots/test_single_file/seed=all_model={model}_split=all.pdf")
-    print(f"Done!!!")
+    path = f"plots/test_single_file/{feature_learning}/seed=all_model={model}_split=all.pdf"
+    plt.savefig(path)
+    print(f"Saved figure to {path}.")
     
 
 
