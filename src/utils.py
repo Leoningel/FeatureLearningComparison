76e71e28266d23583a872fc398582dc92ee3d532
Leon Ingelse <leoningelse@gmail.com>
2022-07-03 11:28:52 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ import global_vars as gv
 
 def make_grid_search_ready(pipeline:Pipeline):
     if "feature_learning__n_generations" in pipeline.get_params():
-        pipeline.set_params(feature_learning__n_generations=25)
+        pipeline.set_params(feature_learning__n_generations=15)
     return pipeline
 
 def make_evaluation_ready(pipeline:Pipeline, csv_text=''):
