94d2f90ce37b32c22ed9ab89679bbeba961b24ae
Leon Ingelse <leoningelse@gmail.com>
2022-07-05 14:12:23 +0100

    Update m3gp_jb.py

--- src/feature_preparation/search_based/m3gp_jb.py
+++ src/feature_preparation/search_based/m3gp_jb.py
@@ -14,7 +14,7 @@ import global_vars as gv
 name = __name__.split(".")[-1]
 
 class M3GP_JB_Method(BaseEstimator, TransformerMixin):
-    def __init__(self, seed = 0, max_depth=gv.MAX_DEPTH - 3, elitism_size=5, novelties_size=5, prob_mutation=0.01, prob_crossover=0.9, n_generations=500, save_to_csv='') -> None:
+    def __init__(self, seed = 0, max_depth=gv.MAX_DEPTH - 2, elitism_size=5, novelties_size=5, prob_mutation=0.01, prob_crossover=0.9, n_generations=500, save_to_csv='') -> None:
         self.feature_mapping = None
         self.seed = seed
         self.max_depth = max_depth
