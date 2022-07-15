0fa7f6b5e366346317695a029b3b7a16b228020d
Leon Ingelse <leoningelse@gmail.com>
2022-07-15 18:23:31 +0100

    Split up plots

--- plot_search_based.sh
+++ plot_search_based.sh
@@ -15,6 +15,3 @@ run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn franklin -t -g -out search_me
 run_comparison.sh -fl 0 1 2 3 5 -m 0 --plot_data -fn franklin -g -out search_methods_full
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn franklin -t -v -out search_methods_full
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn franklin -v -out search_methods_full
-
-run_comparison.sh -fl 0 5 6 7 8 -m 0 --plot_data -fn franklin -t -v -out compare_w_classics -p 0
-run_comparison.sh -fl 0 5 6 7 8 -m 0 --plot_data -fn franklin -v -out compare_w_classics -p 0
