81c22928440d348954b7f1b80ff248a2d41a0c0f
Leon Ingelse <leoningelse@gmail.com>
2022-07-18 14:28:47 +0100

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -15,7 +15,6 @@ import pandas as pd
 
 import global_vars as gv
 from data_extraction.data_extraction import load
-from evaluation.statistical_tests.simple_tests import kolmogorov_smirnov
 from evaluation.visualization.specifics import visualise_all_seeds_compare_splits, visualise_compare_fls, visualise_single_file, visualise_all_seeds, visualise_all_seeds_all_splits
 from evaluation.visualization.complete import plot_combined_barplot_comparison, plot_separated_violin_comparisons
 from feature_preparation.core import FeatureLearningMethod, FeatureLearningOptimization
