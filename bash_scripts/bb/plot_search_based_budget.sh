a5ee06a07e2a37626810fa3b5048884564eda022
Leon Ingelse <leoningelse@gmail.com>
2022-07-19 14:26:37 +0100

    Update plot_search_based_budget.sh

--- bash_scripts/bb/plot_search_based_budget.sh
+++ bash_scripts/bb/plot_search_based_budget.sh
@@ -8,9 +8,9 @@ function run_example {
 
 }
 
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn bb/time -t -g -out timed_search_methods -pt
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn bb/time -g -out timed_search_methods -pt
+run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn bb/time -t -g -out timed_search_methods
+run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn bb/time -g -out timed_search_methods
 
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -t -g -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -t -g -out timed_search_methods_full
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -t -v -out timed_search_methods_full -pt
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -v -out timed_search_methods_full -pt
