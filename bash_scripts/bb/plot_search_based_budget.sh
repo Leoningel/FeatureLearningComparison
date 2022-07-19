bf8b746fe3ca033b15aae1c9b8c1b0b016abdd9b
Leon Ingelse <leoningelse@gmail.com>
2022-07-19 17:38:40 +0100

    Update plot_search_based_budget.sh

--- bash_scripts/bb/plot_search_based_budget.sh
+++ bash_scripts/bb/plot_search_based_budget.sh
@@ -14,3 +14,9 @@ run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn bb/time -g -out timed_search_me
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -t -g -out timed_search_methods_full
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -t -v -out timed_search_methods_full -pt
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn bb/time -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn bb/time -t -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn bb/time -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn bb/time -t -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn bb/time -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn bb/time -t -v -out timed_search_methods_full -pt
+run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn bb/time -v -out timed_search_methods_full -pt
