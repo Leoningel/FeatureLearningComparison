f04b4625dbac8a6437bab9fd121ef1a1ac547b52
Leon Ingelse <leoningelse@gmail.com>
2022-07-20 11:03:56 +0100

    Update plot_all_methods.sh

--- bash_scripts/bb/plot_all_methods.sh
+++ bash_scripts/bb/plot_all_methods.sh
@@ -8,9 +8,11 @@ function run_example {
 
 }
 
-run_comparison.sh -fl 0 2 5 6 7 8 -m 0 1 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 0 1 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0 -o
+run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0 -o
 run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn bb/max_depth_12 -t -v -out compare_w_classics -p 0
 run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn bb/max_depth_12 -v -out compare_w_classics -p 0
