324254b81216e9943cbd3ce59c69346534ee488f
Leon Ingelse <leoningelse@gmail.com>
2022-07-21 11:59:40 +0100

    Update plot_search_based.sh

--- bash_scripts/credit/plot_search_based.sh
+++ bash_scripts/credit/plot_search_based.sh
@@ -15,7 +15,7 @@ run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out 
 run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods
 
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out search_methods_full
-run_comparison.sh -fl 0 2 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods_full
+run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods_full
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
 run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
 run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
