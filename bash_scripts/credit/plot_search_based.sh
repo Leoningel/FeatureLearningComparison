1a2a736751655b1c108dd1f1ef8964be65c25003
Leon Ingelse <leoningelse@gmail.com>
2022-07-21 11:50:45 +0100

    bash scripts

--- bash_scripts/credit/plot_search_based.sh
+++ bash_scripts/credit/plot_search_based.sh
@@ -8,19 +8,19 @@ function run_example {
 
 }
 
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 --time -v -out search_methods_full -p 2 -o
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 --nodes -g -out search_methods_full -p -1 -o
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 --time -v -out search_methods_full -p 2 -o
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 --nodes -g -out search_methods_full -p -1 -o
 
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out search_methods
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods
+# run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out search_methods
+# run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods
 
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out search_methods_full
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods_full
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
-run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
-run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
-run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2 -o
-run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2 -o
-run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
-run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out search_methods_full
+run_comparison.sh -fl 0 2 -m 0 --plot_data -fn credit/max_depth_12 -g -out search_methods_full
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2 -o
+# run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2 -o
+# run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/max_depth_12 -t -v -out search_methods_full -p 2
+# run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/max_depth_12 -v -out search_methods_full -p 2
