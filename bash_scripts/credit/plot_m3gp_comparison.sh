d29b349d5a61f90ca428af9f8ffeb6cb396de0ce
Leon Ingelse <leoningelse@gmail.com>
2022-07-23 02:13:14 +0100

    plots

--- bash_scripts/credit/plot_m3gp_comparison.sh
+++ bash_scripts/credit/plot_m3gp_comparison.sh
@@ -8,8 +8,6 @@ function run_example {
 
 }
 
-run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 --nodes -g -out jb_gengy_depth_12 -p -1 -o
-run_comparison.sh -fl 2 4 -m 0 1 2 3 --plot_data -fn credit/max_depth_12 --time -v -out jb_gengy_depth_12 -p -1 -o
 run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 -t -v -out jb_gengy_depth_12 -p -1 -o
 run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 -v -out jb_gengy_depth_12 -p -1 -o
 run_comparison.sh -fl 2 4 -m 1 --plot_data -fn credit/max_depth_12 -t -v -out jb_gengy_depth_12 -p -1 -o
@@ -20,3 +18,6 @@ run_comparison.sh -fl 2 4 -m 3 --plot_data -fn credit/max_depth_12 -t -v -out jb
 run_comparison.sh -fl 2 4 -m 3 --plot_data -fn credit/max_depth_12 -v -out jb_gengy_depth_12 -p -1 -o
 run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 -t -g -out jb_gengy_depth_12 -p -1 -o
 run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 -g -out jb_gengy_depth_12 -p -1 -o
+
+run_comparison.sh -fl 2 4 -m 0 --plot_data -fn credit/max_depth_12 --nodes -g -out jb_gengy_depth_12 -p -1 -o
+run_comparison.sh -fl 2 4 -m 0 1 2 3 --plot_data -fn credit/max_depth_12 --time -v -out jb_gengy_depth_12 -p -1 -o
