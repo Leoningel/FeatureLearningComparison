8176723d7026a0bc1ac20d133faf656415b21c63
Leon Ingelse <leoningelse@gmail.com>
2022-07-27 11:41:06 +0100

    Update plot_on_budget.sh

--- bash_scripts/credit/plot_on_budget.sh
+++ bash_scripts/credit/plot_on_budget.sh
@@ -4,24 +4,24 @@ PYTHON_BINARY=python
 
 
 
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/time -t -g -out timed_search_methods
-run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/time -g -out timed_search_methods
+# run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/time -t -g -out timed_search_methods
+# run_comparison.sh -fl 0 1 2 -m 0 --plot_data -fn credit/time -g -out timed_search_methods
 
-run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0 -o
-run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0 -o
-run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0
-run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0
+run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0 --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 0 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0 --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0 --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 1 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0 --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0 -o --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 2 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0 -o --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn credit/time -t -v -out timed_compare_w_classics -p 0 --f_score
+run_comparison.sh -fl 0 2 5 6 7 8 -m 3 --plot_data -fn credit/time -v -out timed_compare_w_classics -p 0 --f_score
 
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -t -g -out timed_search_methods_full
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
-run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -t -g -out timed_search_methods_full
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 0 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 1 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 2 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/time -t -v -out timed_search_methods_full -pt
+# run_comparison.sh -fl 0 1 2 3 -m 3 --plot_data -fn credit/time -v -out timed_search_methods_full -pt
