a87d0653ff5318206cd145b779573719ab1aeeed
Eduardo Madeira <eduardomadeira@MacBook-Pro-de-Eduardo.local>
2022-09-16 15:41:27 +0100

    returning visitors column added to daily_website_visitors2.csv

--- data/daily_website_visitors/aux.py
+++ data/daily_website_visitors/aux.py
@@ -8,8 +8,8 @@ with open('daily-website-visitors.csv', 'r') as inp, open('daily-website-visitor
 
     header = next(csv.reader(inp))
     
-    writer.writerow((header[0], header[2], header[3],  header[3], header[4]))
+    writer.writerow((header[0], header[2], "Month",  "Day", header[-1], """target"""))
 
     for row in csv.reader(inp):
-        writer.writerow((row[0], row[2], row[3].split(
-            "/")[0], row[3].split("/")[1], row[4].replace(",", "")))
+        writer.writerow((row[0], row[2], row[3].split("/")[0], 
+                         row[3].split("/")[1], row[-1].replace(",", ""), row[4].replace(",", "")))
